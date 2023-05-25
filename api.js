86c9622867a9d6af2cdf9229027ae57851606fbc
Juliano <julianofirme23@gmail.com>
2023-05-25 13:15:00 -0300

    feat: create the login route with unauthorized error

--- api.js
+++ api.js
@@ -1,6 +1,29 @@
+import { once } from "node:events";
 import { createServer } from "node:http";
 
+const validUser = {
+  user: "juliano",
+  password: "123",
+};
+
+async function loginRoute(req, res) {
+  const { user, password } = JSON.parse(await once(req, "data"));
+  console.log('passou')
+
+  if (user !== validUser.user || password !== validUser.password) {
+    res.writeHead(401);
+    res.end(JSON.stringify({ error: "wrong credentials" }));
+    return;
+  }
+
+  res.end("ok");
+}
+
 async function handler(req, res) {
+  if (req.url === "/login" && req.method === "POST") {
+    return loginRoute(req, res);
+  }
+
   res.end("hello world");
 }
 
