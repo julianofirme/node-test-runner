62fe30c8dc63b62ba10d538e36f540e5f1aa2f87
Juliano <julianofirme23@gmail.com>
2023-05-25 13:20:24 -0300

    test: return 200 with correct credentials

--- api.test.js
+++ api.test.js
@@ -1,7 +1,7 @@
 import { describe, before, after, it } from "node:test";
-import { deepStrictEqual, strictEqual } from "node:assert";
+import { deepStrictEqual, ok } from "node:assert";
 
-const BASE_URL = "http://localhost:3000"
+const BASE_URL = "http://localhost:3000";
 
 describe("API Workflow", () => {
   let _server = {};
@@ -12,19 +12,35 @@ describe("API Workflow", () => {
 
   after((done) => _server.close(done));
 
-  it("should receive not authorized given wrong credentials", async () => {
+  it("should receive not authorized when given wrong credentials", async () => {
     const data = {
-      user: 'juliano',
-      password: 'secret1234'
-    }
+      user: "juliano",
+      password: "secret1234",
+    };
 
     const request = await fetch(`${BASE_URL}/login`, {
-      method: 'POST',
-      body: JSON.stringify(data)
-    })
-    strictEqual(request.status, 401)
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    deepStrictEqual(request.status, 401);
 
-    const response = await request.json()
-    deepStrictEqual(response, { error: 'wrong credentials' })
-  })
+    const response = await request.json();
+    deepStrictEqual(response, { error: "wrong credentials" });
+  });
+
+  it("should login successfuly when given correct credentials", async () => {
+    const data = {
+      user: "juliano",
+      password: "123",
+    };
+
+    const request = await fetch(`${BASE_URL}/login`, {
+      method: "POST",
+      body: JSON.stringify(data),
+    });
+    deepStrictEqual(request.status, 200);
+
+    const response = await request.json();
+    ok(response.token, 'token should be present')
+  });
 });
