80ac648af562c533e47ec2451e1d61226dae8358
Juliano <julianofirme23@gmail.com>
2023-05-25 11:49:48 -0300

    test: return 401 with invalid credentials

--- api.test.js
+++ api.test.js
@@ -1,4 +1,7 @@
 import { describe, before, after, it } from "node:test";
+import { deepStrictEqual, strictEqual } from "node:assert";
+
+const BASE_URL = "http://localhost:3000"
 
 describe("API Workflow", () => {
   let _server = {};
@@ -8,4 +11,20 @@ describe("API Workflow", () => {
   });
 
   after((done) => _server.close(done));
+
+  it("should receive not authorized given wrong credentials", async () => {
+    const data = {
+      user: 'juliano',
+      password: 'secret1234'
+    }
+
+    const request = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      body: JSON.stringify(data)
+    })
+    strictEqual(request.status, 401)
+
+    const response = await request.json()
+    deepStrictEqual(response, { error: 'wrong credentials' })
+  })
 });
